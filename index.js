7221bc868083d8b36141472c8ba601b512ce6e2e
Dmitrii Baklai <70129614+baklai@users.noreply.github.com>
2025-03-29 21:22:15 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,109 +1,227 @@
-import { select } from '@inquirer/prompts';
-import chalk from 'chalk';
-import dotenv from 'dotenv';
+import fs from 'node:fs/promises';
+import readline from 'node:readline/promises';
+import OpenAI from 'openai';
 
-import GPTAgent from './src/index.js';
+import MCPHost from './mcp/index.js';
 
-dotenv.config();
+import { truncate } from './utils/index.js';
 
-const { OPENAI_API_KEY, OPENAI_API_MODEL } = process.env;
+const SYSTEM_MESSAGE_PROMPT = {
+  role: 'system',
+  content: `You are a helpful assistant-agent who supports your user. You are well acquainted with your tools and resources and use them to solve the user's problems. When asked to complete a task, you perform as many actions autonomously as possible (e.g., logging into a system, opening a file, making a request, etc.). The only exception to this rule is tasks that could be potentially destructive. If you can achieve the goal directly, you return the result. If the task cannot be completed, you explain the reason. Today's date is: ${
+    new Date().toISOString().split('T')[0]
+  }`
+};
 
-if (!OPENAI_API_KEY) {
-  throw new Error('OpenAI API Key is not set');
+const GPT_AGENT_MESSAGE = `
+ █▀▀█  █▀▀█ ▀▀█▀▀ 　  █▀▀█  █▀▀█  █▀▀▀  █▄  █ ▀▀█▀▀ 　  █▀▀█  █    ▀█▀ 
+▒█░▄▄ ▒█▄▄█ ░▒█░░ 　 ▒█▄▄█ ▒█░▄▄ ▒█▀▀▀ ▒█▒█▒█ ░▒█░░ 　 ▒█░░░ ▒█░░░ ▒█░ 
+▒█▄▄█ ▒█░░░ ░▒█░░ 　 ▒█░▒█ ▒█▄▄█ ▒█▄▄▄ ▒█░░▀█ ░▒█░░ 　 ▒█▄▄█ ▒█▄▄█ ▄█▄
+`;
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-if (!OPENAI_API_MODEL) {
-  throw new Error('OpenAI Model is not set');
+async function prinAndAnimation(prefix, text, delay = 100) {
+  process.stdout.write(prefix);
+  for (let i = 0; i < text.length; i++) {
+    process.stdout.write(text[i]);
+    await sleep(delay);
+  }
+  console.log();
 }
 
-async function main() {
-  const gptAgent = new GPTAgent({
-    apiKey: OPENAI_API_KEY,
-    apiModel: OPENAI_API_MODEL
-  });
+export default class GPTAgent {
+  model;
+  openai;
+  mcphost = null;
+  messages = [];
 
-  try {
-    await gptAgent.readConfig();
+  constructor({ apiKey, apiModel = 'gpt-3.5-turbo' }) {
+    this.model = apiModel;
+    this.openai = new OpenAI({ apiKey: apiKey });
+    this.mcphost = new MCPHost({ name: 'gpt-mcp-host', version: '1.0.0' });
 
-    while (true) {
-      console.log('\n');
+    console.log('\x1b[32m' + GPT_AGENT_MESSAGE + '\x1b[0m');
+  }
 
-      const answer = await select({
-        message: chalk.green('Select a menu item'.toUpperCase()),
-        choices: [
-          {
-            name: `${chalk.green('•')} Open the chat`,
-            value: async () => {
-              console.clear();
-              const message = 'GPT AGENT CLI';
-              console.log(chalk.green(message));
-              await gptAgent.chatLoop();
-            }
-          },
-          {
-            name: `${chalk.green('•')} View MCP Tools`,
-            value: async () => {
-              console.clear();
-              const message = 'MCP Tools';
-              console.log(`\n${chalk.green(message.toUpperCase())}`);
-              const listServers = await gptAgent.viewMCPTolls();
-              console.table(listServers);
-            }
-          },
-          {
-            name: `${chalk.green('•')} View history of chat`,
-            value: async () => {
-              console.clear();
-              const message = 'History of chat';
-              console.log(`\n${chalk.green(message.toUpperCase())}`);
-              const messages = await gptAgent.chatHistoryView();
-              if (messages.length) {
-                console.table(messages);
-              } else {
-                console.log(chalk.gray('\nNot Found'));
-              }
-            }
-          },
-          {
-            name: `${chalk.green('•')} Clean a history of chat`,
-            value: async () => {
-              console.clear();
-              gptAgent.chatHistoryClear();
-              const message = 'Cleaned a history of chat';
-              console.log(`\n${chalk.gray(message)}`);
-            }
-          },
-          {
-            name: `${chalk.green('•')} Save a history of chat`,
-            value: async () => {
-              console.clear();
-              await gptAgent.chatHistorySave();
-              const message = 'The chat is saved to a file';
-              console.log(`\n${chalk.gray(message)}`);
-            }
-          },
-          {
-            name: `${chalk.green('•')} Exit the GPT Agent`,
-            value: async () => {
-              console.clear();
-              await gptAgent.cleanup();
-              process.exit(0);
-            }
+  async readConfig(configFilePath = 'agent-config.json') {
+    try {
+      await fs.access(configFilePath);
+    } catch (err) {
+      await fs.writeFile(configFilePath, JSON.stringify({ mcpServers: {} }, null, 2), 'utf8');
+    }
+
+    try {
+      const data = await fs.readFile(configFilePath, 'utf8');
+      const { mcpServers } = JSON.parse(data);
+
+      for (const server in mcpServers) {
+        await this.mcphost.connect(server, mcpServers[server].command, mcpServers[server].args);
+      }
+    } catch (err) {
+      console.error('Failed init model context protocol:', err);
+    }
+  }
+
+  async processQuery(query) {
+    this.messages.push({ role: 'user', content: query });
+
+    const listTools = await this.mcpTools();
+
+    const response = await this.openai.chat.completions.create({
+      model: this.model,
+      messages: this.messages,
+      temperature: 0.2,
+      tools: listTools
+    });
+
+    const finalText = [];
+    const toolResults = [];
+
+    for (const choice of response.choices) {
+      if (choice.message.role === 'assistant' && choice.finish_reason === 'stop') {
+        finalText.push(choice.message.content);
+        this.messages.push({
+          role: 'assistant',
+          content: choice.message.content
+        });
+      } else if (
+        choice.message.role === 'assistant' &&
+        (choice.message?.tool_calls?.length || choice.finish_reason === 'tool_calls')
+      ) {
+        for (const tool of choice.message.tool_calls) {
+          if (!tool?.function?.name) {
+            continue;
           }
-        ]
-      });
 
-      if (typeof answer === 'function') {
-        await answer();
-      } else {
-        console.error(`Function ${answer} does not exist.`);
+          const toolName = tool.function.name;
+          const toolArgs = JSON.parse(tool.function.arguments);
+
+          const toolResult = await this.mcphost.callTool(toolName, toolArgs);
+
+          toolResults.push({ role: 'user', content: toolResult.content });
+
+          finalText.push(
+            `\n\x1b[90m • Calling tool ${toolName} with args ${JSON.stringify(toolArgs)}\x1b[0m\n`
+          );
+        }
+
+        this.messages.push(...toolResults);
+
+        const response = await this.openai.chat.completions.create({
+          model: this.model,
+          temperature: 0.2,
+          messages: this.messages
+        });
+
+        this.messages.push({
+          role: 'assistant',
+          content: response.choices[0].message.content
+        });
+
+        finalText.push(
+          response.choices[0].message.role === 'assistant' &&
+            response.choices[0].finish_reason === 'stop'
+            ? response.choices[0].message.content
+            : ''
+        );
       }
     }
-  } catch (err) {
-    console.clear();
-    console.error('Undeclared error:', err);
-    process.exit(0);
+
+    return finalText.join('\n');
+  }
+
+  async mcpTools() {
+    const listTools = await this.mcphost.listTools();
+
+    return listTools.map(tool => {
+      return {
+        type: 'function',
+        function: {
+          name: tool.name,
+          description: tool.description,
+          parameters: tool.inputSchema
+        }
+      };
+    });
+  }
+
+  async chatLoop() {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      terminal: false
+    });
+
+    this.messages = [{ ...SYSTEM_MESSAGE_PROMPT }];
+
+    try {
+      console.log('\x1b[90m' + "Type your queries or 'quit/exit' to exit." + '\x1b[0m');
+
+      while (true) {
+        const message = await rl.question('\n' + '\x1b[32m' + 'Query: ' + '\x1b[0m');
+
+        if (message.toLowerCase() === 'quit' || message.toLowerCase() === 'exit') {
+          break;
+        }
+
+        const response = await this.processQuery(message);
+
+        await prinAndAnimation('\n\x1b[34mAssistant:\x1b[0m ', response, 150);
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      rl.close();
+    }
+  }
+
+  async chatHistoryView() {
+    return this.messages
+      .filter(msg => msg.role === 'user' || msg.role === 'assistant')
+      .map(msg => {
+        const role = msg.role === 'user' ? '\x1b[32mUser:\x1b[0m' : '\x1b[34mAssistant:\x1b[0m';
+        return `${role} \x1b[90m${msg.content}\x1b[0m`;
+      })
+      .join('\n');
+  }
+
+  async chatHistorySave() {
+    const datetime = new Date();
+
+    const timestamp = datetime.toISOString().replace(/[T:.-]/g, '_');
+
+    const filename = `chat_history_${timestamp}.md`;
+
+    const markdownContent = this.messages
+      .filter(msg => msg.role === 'user' || msg.role === 'assistant')
+      .map(msg => {
+        const role = msg.role === 'user' ? '**User:**' : '**Assistant:**';
+        return `${role} ${msg.content}`;
+      })
+      .join('\n\n---\n\n');
+
+    await fs.writeFile(filename, markdownContent, 'utf-8');
+  }
+
+  async viewMCPTolls() {
+    const listTools = await this.mcphost.listTools();
+
+    return listTools.map(tool => {
+      return {
+        name: tool.name,
+        description: truncate(tool.description, 80)
+      };
+    });
+  }
+
+  chatHistoryClear() {
+    this.messages = [{ ...SYSTEM_MESSAGE_PROMPT }];
+  }
+
+  async cleanup() {
+    await this.mcphost.close();
   }
 }
-
-main();
